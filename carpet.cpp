bb745f6711752c008782d7eef14b8965bf969574
qucker <qucker135@gmail.com>
2020-09-16 13:25:31 +0200

    Translate code to english

--- carpet.cpp
+++ carpet.cpp
@@ -1,58 +1,51 @@
 #include <iostream>
 #include <fstream>
-//#include <math.h>
 
 using namespace std;
 
-unsigned int pow(unsigned int,unsigned int);
+unsigned pow(unsigned,unsigned);
 
-bool znak(unsigned int ro,unsigned int st){
+bool sign(unsigned ro,unsigned st){
 	if(st==0) return true; //1
 	else if(ro>pow(3,st-1) && ro<=2*pow(3,st-1)) return false;//0
-	else return znak((ro+pow(3,st-1)-1)%pow(3,st-1)+1,st-1);
+	else return sign((ro+pow(3,st-1)-1)%pow(3,st-1)+1,st-1);
 }
 
-bool znak2(unsigned int r,unsigned int k,unsigned int st){
+bool sign2(unsigned r,unsigned k,unsigned st){
 	if(st==0) return true; //1
 	else if(r>pow(3,st-1) && r<=2*pow(3,st-1) && k>pow(3,st-1) && k<=2*pow(3,st-1)) return false;//0
-	else return znak2((r+pow(3,st-1)-1)%pow(3,st-1)+1,(k+pow(3,st-1)-1)%pow(3,st-1)+1,st-1);
+	else return sign2((r+pow(3,st-1)-1)%pow(3,st-1)+1,(k+pow(3,st-1)-1)%pow(3,st-1)+1,st-1);
 }
 
-unsigned int pow(unsigned int b,unsigned int w){
-	unsigned int r0 = 1;
-	for(unsigned int i=1;i<=w;i++) r0*=b;
+unsigned pow(unsigned b,unsigned w){
+	unsigned r0 = 1;
+	for(unsigned i=1;i<=w;i++) r0*=b;
 	return r0;
 }
 
 int main(){
 	string st;
-	cout<<"Podaj stopien rekurencji: ";
+	cout<<"Enter degree of recursion (<10 - safe): ";
 	cin>>st;
-	unsigned int stl = atoi(st.c_str());
-	ofstream plik("dywan"+st+".pbm");
-	plik<<"P1\n"<<pow(3,stl)<<" "<<pow(3,stl)<<"\n";
+	unsigned stl = atoi(st.c_str());
+	ofstream file("carpet"+st+".pbm");
+	file<<"P1\n"<<pow(3,stl)<<" "<<pow(3,stl)<<"\n";
 	if(stl>9){
 		stl=9; st="9";
 	}
-	const unsigned int W = pow(3,stl);
-	const unsigned int H = W;
-	const unsigned int duzaL = pow(3,2*stl-1);
-	for(unsigned int i=0;i<duzaL;i++){
-		for(unsigned int j=1;j<=3;j++){
-			unsigned int poz = 3*i+j;
-			unsigned int r = ((poz+W-1)/W);
-			unsigned int k = poz - (r-1)*W;
-			//r+=1;k+=1;
-			if(!znak2(r,k,stl)) plik<<"0";
-			else plik<<"1";
+	const unsigned W = pow(3,stl);
+	const unsigned H = W;
+	const unsigned bigL = pow(3,2*stl-1);
+	for(unsigned i=0;i<bigL;i++){
+		for(unsigned j=1;j<=3;j++){
+			unsigned poz = 3*i+j;
+			unsigned r = ((poz+W-1)/W);
+			unsigned k = poz - (r-1)*W;
+			if(!sign2(r,k,stl)) file<<"0";
+			else file<<"1";
 		}
-		plik<<"\n";
+		file<<"\n";
 	}
-	plik.close();
-	//cout<<pow(2,3)<<endl;
-	/*unsigned int st;
-	cin>>st;
-	//const unsigned int st=3;
-	for(unsigned int i=1;i<=pow(3,st);i++) cout<<znak(i,st);*/
+	file.close();
 	return 0;
 }
--- oilcloth.cpp
+++ oilcloth.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <fstream>
-//#include <math.h>
 
 using namespace std;
 
 int pow(int,int);
 
-bool znak(int ro,int st){
+bool sign(int ro,int st){
 	if(st==0) return true; //1
 	else if(ro>pow(3,st-1) && ro<=2*pow(3,st-1)) return false;//0
-	else return znak((ro+pow(3,st-1)-1)%pow(3,st-1)+1,st-1);
+	else return sign((ro+pow(3,st-1)-1)%pow(3,st-1)+1,st-1);
 }
 
 int pow(int b,int w){
@@ -20,33 +19,28 @@ int pow(int b,int w){
 
 int main(){
 	string st;
-	cout<<"Podaj stopien rekurencji: ";
+	cout<<"Enter degree of recursion (<10 - safe): ";
 	cin>>st;
 	int stl = atoi(st.c_str());
-	ofstream plik("cerata"+st+".pbm");
-	plik<<"P1\n"<<pow(3,stl)<<" "<<pow(3,stl)<<"\n";
+	ofstream file("oilcloth"+st+".pbm");
+	file<<"P1\n"<<pow(3,stl)<<" "<<pow(3,stl)<<"\n";
 	if(stl>10){
 		stl=10; st="10";
 	}
 	const int W = pow(3,stl);
 	const int H = W;
-	const int duzaL = pow(3,2*stl-1);
-	for(int i=0;i<duzaL;i++){
+	const int bigL = pow(3,2*stl-1);
+	for(int i=0;i<bigL;i++){
 		for(int j=1;j<=3;j++){
 			int poz = 3*i+j;
 			int r = ((poz+W-1)/W);
 			int k = poz - (r-1)*W;
-			//r+=1;k+=1;
-			if((!znak(r,stl)) && (!znak(k,stl))) plik<<"0";
-			else plik<<"1";
+			
+			if((!sign(r,stl)) && (!sign(k,stl))) file<<"0";
+			else file<<"1";
 		}
-		plik<<"\n";
+		file<<"\n";
 	}
-	plik.close();
-	//cout<<pow(2,3)<<endl;
-	/*int st;
-	cin>>st;
-	//const int st=3;
-	for(int i=1;i<=pow(3,st);i++) cout<<znak(i,st);*/
+	file.close();
 	return 0;
 }
